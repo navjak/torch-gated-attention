1f0e4721bd926a75ea0281cec0686f6e2acd7b65
navjak <navdeepjakhar30@gmail.com>
2025-12-12 16:54:48 +0530

    added core gated sdpa logic

---
+++ gated_attention/core.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.Functional as F
+from typing import Optional
+
+def gated_sdpa(q: torch.Tensor, 
+               k: torch.Tensor, 
+               v: torch.Tensor,
+               gate_w: torch.Tensor,
+               gate_b: Optional[torch.Tensor]= None,
+               attn_mask: Optional[torch.Tensor]= None,
+               dropout_p: float = 0.0,
+               is_causal: bool = False,
+               scale: Optional[float] = None
+               ) -> torch.Tensor:
+    
+    """
+    Calculates the Gated Scaled Dot Product Attention (Qiu et al., 2025).
+    """
+    # PyTorch SDPA (Flash attn handled internally)
+    attn_out = F.scaled_dot_product_attention(
+        q,k,v,
+        attn_mask = attn_mask,
+        dropout_p = dropout_p, 
+        is_causal = is_causal,
+        scale = scale
+        )
+    
+    # Gate
+    gate_logits = F.linear(q, gate_w, gate_b)
+    gate_score = torch.sigmoid(gate_logits)
+
+    return attn_out * gate_score
